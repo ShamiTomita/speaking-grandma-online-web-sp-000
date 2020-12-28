ca7963a9c39f2f9e267f427b2d06cad444d2d387
Shami Tomita <shamitomita@gmail.com>
2020-12-28 21:31:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif  
     return "HUH?! SPEAK UP, SONNY!"
+  end
 end 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
