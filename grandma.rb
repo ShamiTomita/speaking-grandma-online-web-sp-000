cf5ca0b5cd4b6065760ed1daf16a4891fe169dac
Shami Tomita <shamitomita@gmail.com>
2020-12-28 21:33:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  end
-  if phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else  
     return "HUH?! SPEAK UP, SONNY!"
@@ -15,7 +14,6 @@ end
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
 
 # NO, NOT SINCE 1938!
 
